8ee21f1ed1f975c0ef8858e6808c5fb17604c50b
ROGERIO CARVALHO <rogerio_carvalho15@hotmail.com>
2015-06-14 16:23:05 -0300

    metdos get, post , delete,  implementados

--- ControleNutricionalClient/Controllers/AlimentoController.cs
+++ ControleNutricionalClient/Controllers/AlimentoController.cs
@@ -24,18 +24,65 @@ namespace ControleNutricionalClient.Controllers
         }
 
         // POST api/alimento
-        public void Post([FromBody]string value)
-        {
+        public HttpResponseMessage Post(Alimento alimento){
+            if (ModelState.IsValid) {
+
+                try {
+                    ServiceAlimento.ServiceAlimentoClient servico = new ServiceAlimentoClient();
+                    servico.create(alimento);
+                    HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, alimento);
+                    response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = alimento.Id }));
+                    return response;
+                }
+                catch (Exception ex) {
+                     return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+                }
+                
+            }
+            else {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
         }
 
         // PUT api/alimento/5
-        public void Put(int id, [FromBody]string value)
-        {
+        public HttpResponseMessage Put(int id, Alimento alimento){
+            if (!ModelState.IsValid) {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+
+            if (id != alimento.Id) {
+                return Request.CreateResponse(HttpStatusCode.BadRequest);
+            }
+            
+           // db.Entry(todo).State = EntityState.Modified;
+
+            try {
+                ServiceAlimento.ServiceAlimentoClient servico = new ServiceAlimentoClient();
+                servico.edit(alimento);
+            }
+            catch (Exception ex) {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);
         }
 
         // DELETE api/alimento/5
-        public void Delete(int id)
-        {
+        public HttpResponseMessage Delete(Alimento alimento) {
+            //Alimento a = db.Alimento.Find(id);
+            if (alimento == null) {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+
+            try {
+                ServiceAlimento.ServiceAlimentoClient servico = new ServiceAlimentoClient();
+                servico.delete(alimento);
+            }
+            catch (Exception ex) {
+                return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, alimento);
         }
     }
 }
