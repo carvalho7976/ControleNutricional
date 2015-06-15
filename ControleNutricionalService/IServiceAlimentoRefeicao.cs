05409c27758adc3804440840cf32723d55870dd6
Lucas Araujo <lucas.compufc@gmail.com>
2015-06-15 01:20:43 -0300

    Crud Alimento Refeicao

---
+++ ControleNutricionalService/IServiceAlimentoRefeicao.cs
@@ -0,0 +1,37 @@
+﻿using ControleNutricionalService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ControleNutricionalService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IServiceAlimentoRefeicao" in both code and config file together.
+    [ServiceContract]
+    public interface IServiceAlimentoRefeicao
+    {
+        //Crud para AlimentoRefeicao
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "findall", ResponseFormat = WebMessageFormat.Json)]
+        List<AlimentoRefeicao> findall();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "find/{id}", ResponseFormat = WebMessageFormat.Json)]
+        AlimentoRefeicao find(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "create", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        bool create(AlimentoRefeicao alimentoRefeicao);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", UriTemplate = "edit", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        bool edit(AlimentoRefeicao alimentoRefeicao);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", UriTemplate = "delete", ResponseFormat = WebMessageFormat.Json, RequestFormat = WebMessageFormat.Json)]
+        bool delete(AlimentoRefeicao alimentoRefeicao);
+    }
+}
---
+++ ControleNutricionalService/ServiceAlimentoRefeicao.svc.cs
@@ -0,0 +1,93 @@
+﻿using ControleNutricionalService.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ControleNutricionalService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceAlimentoRefeicao" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceAlimentoRefeicao.svc or ServiceAlimentoRefeicao.svc.cs at the Solution Explorer and start debugging.
+    //Crud para AlimentoRefeicao
+    public class ServiceAlimentoRefeicao : IServiceAlimentoRefeicao
+    {
+
+        public List<AlimentoRefeicao> findall()
+        {
+            using (NutricaoContext mde = new NutricaoContext())
+            {
+                return mde.AlimentoRefeicao.ToList();
+            };
+        }
+
+        public AlimentoRefeicao find(string id)
+        {
+            using (NutricaoContext mde = new NutricaoContext())
+            {
+                int nid = Convert.ToInt32(id);
+                return mde.AlimentoRefeicao.Where(ae => ae.Id == nid).First();
+            };
+        }
+
+        public bool create(AlimentoRefeicao alimentoRefeicao)
+        {
+            using (NutricaoContext mde = new NutricaoContext())
+            {
+                try
+                {
+                    mde.AlimentoRefeicao.Add(alimentoRefeicao);
+                    mde.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Debug.Write(ex.ToString());
+                    return false;
+                }
+            };
+        }
+
+        public bool edit(AlimentoRefeicao alimentoRefeicao)
+        {
+            using (NutricaoContext mde = new NutricaoContext())
+            {
+                var result = mde.AlimentoRefeicao.SingleOrDefault(r => r.Id == alimentoRefeicao.Id);
+
+                if (result != null)
+                {
+                    //mde.Refeicao.Add(refeicao);                    
+                    mde.Entry(alimentoRefeicao).State = EntityState.Modified;
+                    mde.SaveChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            };
+        }
+
+        public bool delete(AlimentoRefeicao alimentoRefeicao)
+        {
+            using (NutricaoContext mde = new NutricaoContext())
+            {
+                var result = mde.AlimentoRefeicao.Where(rf => rf.Id == alimentoRefeicao.Id).SingleOrDefault();
+                if (result != null)
+                {
+                    mde.AlimentoRefeicao.Remove(result);
+                    mde.SaveChanges();
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            };
+        }
+    }
+}
